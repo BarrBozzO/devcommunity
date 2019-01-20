be354229118a789138cdd3fbaa95d8ad6136f4ea
Konstantin <barrbozzopower@gmail.com>
2019-01-20 17:17:01 +0400

    Prepare for deploy

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
+const path = require("path");
 
 // Project config
 const config = require("./config");
@@ -28,6 +29,16 @@ require("./config/passport.js")(passport);
 app.use("/api/users", users);
 app.use("/api/profile", profile);
 app.use("/api/posts", posts);
+
+// Server static assets if in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(expres.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.on("dbConnected", function() {
   app.listen(process.env.PORT, err => {
